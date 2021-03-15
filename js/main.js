7b56c9a82e29643b99f3d5ef2c859dfa6d96e207
Libia Perez <alexandra220790@gmail.com>
2021-03-15 19:12:48 -0300

    add load more effect in Placement View and News view

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ var myInput = document.getElementById("myInput");
 myModal.addEventListener("shown.bs.modal", function () {
   myInput.focus();
 }); */
+
+/*js for load more*/
+$(document).ready(function () {
+  $(".moreBox").slice(0, 3).show();
+  if ($(".blogBox:hidden").length != 0) {
+    $("#loadMore").show();
+  }
+  $("#loadMore").on("click", function (e) {
+    e.preventDefault();
+    $(".moreBox:hidden").slice(0, 6).slideDown();
+    if ($(".moreBox:hidden").length == 0) {
+      $("#loadMore").fadeOut("slow");
+    }
+  });
+});
